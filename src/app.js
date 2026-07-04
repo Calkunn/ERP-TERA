ec8607a860ea1a3ade7867825a0e952a3b3a1fb0
Calkunn <nathanaelcalvin.a@gmail.com>
2026-07-04 16:52:54 +0700

    fix: make cash budget and income statement sheets compact ONLY in mobile view, restoring spacious desktop view

--- src/app.js
+++ src/app.js
@@ -2440,8 +2440,8 @@ function renderIncomeStatement(data) {
   
   let html = `
     <tr class="header-row">
-      <td style="font-weight:800; min-width: 150px;">Tera</td>
-      ${data.map(m => `<td style="font-weight:800; text-align:right; min-width: 130px;">Tera</td>`).join("")}
+      <td style="font-weight:800;" class="col-label">Tera</td>
+      ${data.map(m => `<td style="font-weight:800; text-align:right;" class="col-data">Tera</td>`).join("")}
     </tr>
     <tr class="header-row">
       <td style="font-weight:700;">Income Statement</td>
@@ -2552,8 +2552,8 @@ function renderCashBudget(data) {
   
   let html = `
     <tr class="header-row">
-      <td style="font-weight:800; min-width: 150px;">Tera</td>
-      ${data.map(m => `<td style="font-weight:800; text-align:right; min-width: 130px;">Tera</td>`).join("")}
+      <td style="font-weight:800;" class="col-label">Tera</td>
+      ${data.map(m => `<td style="font-weight:800; text-align:right;" class="col-data">Tera</td>`).join("")}
     </tr>
     <tr class="header-row">
       <td style="font-weight:700;">Cash Budget</td>
